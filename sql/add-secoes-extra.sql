b3017cf387a27564290a76d7bafe295ec136c391
kaua9999 <fatimazouza4@gmail.com>
2026-07-07 00:28:00 +0000

    removendo antigo

--- sql/add-secoes-extra.sql
+++
@@ -1,7 +0,0 @@
-alter table pedidos add column if not exists carta text;
-alter table pedidos add column if not exists linha_do_tempo text;
-alter table pedidos add column if not exists promessas text;
-
-alter table paginas add column if not exists carta text;
-alter table paginas add column if not exists linha_do_tempo text;
-alter table paginas add column if not exists promessas text;
--- sql/add-slug-publicado.sql
+++
@@ -1,1 +0,0 @@
-alter table pedidos add column if not exists slug_publicado text;
